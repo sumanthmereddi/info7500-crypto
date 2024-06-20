58ff9d5ddd610ce3e4790d8a042a8835db6eca12
Kshama Aditi Lethakula <114813685+kshamaaditilethakula@users.noreply.github.com>
2024-06-20 16:36:30 -0400

    Update App.tsx

--- bitcoin-explorer/src/App.tsx
+++ bitcoin-explorer/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import BlockHeight from './BlockHeight';
- 
+
+
 const App: React.FC = () => {
   return (
-<div className="App">
-<header className="App-header">
-<BlockHeight />
-</header>
-</div>
+    <div className="App">
+      <header className="App-header">
+        <BlockHeight />
+      </header>
+    </div>
   );
 };
- 
+
 export default App;
